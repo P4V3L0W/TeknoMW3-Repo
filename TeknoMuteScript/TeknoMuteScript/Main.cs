b3317825db3aa9e9dd396eaadd6c849414b72735
PAV3LOW <49235615+P4V3L0W@users.noreply.github.com>
2019-06-22 22:32:34 +0200

    MuteScript Init Commit

---
+++ TeknoMuteScript/TeknoMuteScript/Main.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfinityScript;
+
+namespace TeknoMuteScript
+{
+	public class MuteScript : BaseScript
+	{
+		public MuteScript()
+		{
+			PlayerConnected += player => { player.SetField("muted", false); };
+		}
+
+		public override EventEat OnSay3(Entity player, ChatType type, string name, ref string message)
+		{
+			if (player.GetField<bool>("muted"))
+			{
+				Utils.SayTo(player, "You are muted!");
+				return EventEat.EatGame;
+			}
+
+			/************************************************* Commands ***************************************************/
+
+			if (message.StartsWith("!"))
+			{
+				var cmdRaw = message.Substring(1);
+				var cmdName = cmdRaw.Split(' ')[0].ToLower();
+				var cmdParams = cmdRaw.Split(' ').Skip(1).ToArray();
+
+				switch (cmdName)
+				{
+					case "m":
+					case "mute":
+					{
+						if (cmdParams.Length != 1)
+						{
+							player.Call("iprintbold", "Usage: !mute <name>");
+							return EventEat.EatGame;
+						}
+
+						var playerToMute = Utils.FindEntityByName(cmdParams[0], Players);
+
+						if (playerToMute == null)
+						{
+							Utils.SayTo(player, "Found none or multiple players with that name!");
+							break;
+						}
+
+						MutePlayer(playerToMute, player);
+
+						break;
+					}
+					default:
+						Utils.SayTo(player, "Command does not exist!");
+						break;
+				}
+
+				return EventEat.EatGame;
+			}
+
+			return base.OnSay3(player, type, name, ref message);
+		}
+
+		private static void MutePlayer(Entity player, Entity admin)
+		{
+			if (player.GetField<bool>("muted"))
+			{
+				player.SetField("muted", false);
+				Utils.Say(player + " has been ^1muted ^7by " + admin);
+				Utils.SayTo(player, "You have been ^1muted^7!");
+			}
+			else
+			{
+				player.SetField("muted", true);
+				Utils.Say(player + " has been ^2unmuted ^7by " + admin);
+				Utils.SayTo(player, "You have been ^2unmuted^7!");
+			}
+		}
+	}
+}
---
+++ TeknoMuteScript/TeknoMuteScript/Utils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfinityScript;
+
+namespace TeknoMuteScript
+{
+	public class Utils : BaseScript
+	{
+		public static void SayTo(Entity player, string message)
+		{
+			Utilities.RawSayTo(player, "^;[^5PM^;] ^7: " + message);
+		}
+
+		public static void Say(string message)
+		{
+			Utilities.RawSayAll("^;[^5ClanTag^;] ^7: " + message); //Add server message prefix here
+		}
+
+		public static Entity FindEntityByName(string name, List<Entity> players)
+		{
+			List<Entity> matches = new List<Entity>();
+
+			if (name == "")
+			{
+				return null;
+			}
+
+			foreach (var player in players)
+			{
+				if (player.Name.ToLower() == name) //exact match
+				{
+					return player;
+				}
+
+				if (player.Name.ToLower().Contains(name.ToLower()))
+				{
+					matches.Add(player);
+				}
+			}
+
+			return matches.Count == 1 ? matches[0] : null;
+		}
+	}
+}
